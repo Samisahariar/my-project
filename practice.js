74b6801c29a8d5bb0de6b189dbad0da3916daa59
Samisahariar <samisahariar69@gmail.com>
2024-02-01 09:29:25 -0800

    this is the longest session

--- practice.js
+++ practice.js
@@ -1,9 +1,7 @@
-let num= 54;
-while(num > 25){
-    console.log(num);
-    if(num<50){
-        break
-    }
-    num -= 1
-
+const string = "Hint: You should be able to check whether x or X exists. After that, search online how to replace a character in a string."
+let array = string.split(" ")
+for(let i = 0; i < array.length; i++){
+    
 }
+console.log(array.join(""))
+
